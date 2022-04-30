4ca3e36284febbdb76d97699e15bad0483efa279
Peter Ghobrial <peter_ghob@yahoo.ca>
2022-04-30 01:31:58 -0400

    Added a basic command and structure

--- esports_bot.py
+++ esports_bot.py
@@ -6,4 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+bot = commands.Bot(command_prefix='!')  # Bot commands designated by !
+bot.remove_command("help")              # Removes the default help command.
+
+class Commands(commands.Cog):    # Class storing all the commands of the bot
+    @commands.command(name='hello')
+    async def hello(self, ctx):
+        name = ctx.author.name
+        await ctx.send("Hello {}!".format(name))
+
+bot.add_cog(Commands())     # Adding the commands to the bot
+bot.run(TOKEN)              # This runs the bot
+
+
 
