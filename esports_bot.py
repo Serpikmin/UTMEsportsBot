5b079bf044c3f336c5e50c8bd418bc7ce817601f
THLi3 <31786494+THLi3@users.noreply.github.com>
2022-04-30 19:52:56 -0400

    Added a command to call after a player wins a match

--- esports_bot.py
+++ esports_bot.py
@@ -11,7 +11,10 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')  # Bot commands designated by !
 bot.remove_command("help")              # Removes the default help command.
 
-
+players = []
+matches = ()
+nextround = []
+totalgames = 0;
 
 class Commands(commands.Cog):    # Class storing all the commands of the bot
     @commands.command(name='hello')
@@ -20,17 +23,34 @@ class Commands(commands.Cog):    # Class storing all the commands of the bot
         await ctx.send("Hello {}!".format(name))
 
     @commands.command(name='start-t')
-    async def start(self, ctx, brackets=None): # brackets in the form
+    async def start(self, ctx, brackets=None):
         if brackets == None:
             await ctx.send("Usage Format: Discord Name#(numbers)|+")
         else:
             if re.match("^([a-zA-z]+#[0-9][0-9][0-9][0-9]/)+([a-zA-z]+#[0-9][0-9][0-9][0-9])$", brackets):
+                global players
                 players = brackets.split("/")
+                global matches
                 matches = zip(players[::2], players[1::2])
-
-
+                global nextround
+                nextround = [None] * (len(players)/2)
+                await ctx.send("Tournament Started")
             else:
                 await ctx.send("Usage Format: Discord Name#(numbers)|+")
+
+    @commands.command(name='win')
+    async def start(self, ctx, name=None):
+            if matches == ():
+                await ctx.send("There's no tournament")
+            else:
+                if players.index(name):
+                    nextround.append(name)
+                    await ctx.send("Congrats {}, wait for your next match".format(name))
+                else:
+                    await ctx.send("You're not in the tournament")
+            if None not in nextround:
+                await ctx.send("Next round is ready")
+
 
 
 
