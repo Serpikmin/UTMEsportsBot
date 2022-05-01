df2be27db5f6661aef2e77e446f375ba23b84c85
THLi3 <31786494+THLi3@users.noreply.github.com>
2022-04-30 23:58:01 -0400

    Minor bugs fixed

--- esports_bot.py
+++ esports_bot.py
@@ -31,30 +31,35 @@ class Commands(commands.Cog):    # Class storing all the commands of the bot
             if re.match("^([a-zA-z]+#[0-9][0-9][0-9][0-9]/)+([a-zA-z]+#[0-9][0-9][0-9][0-9])$", brackets):
                 global players
                 players = brackets.split("/") # stores players in a list
-                global matches
-                matches = zip(players[::2], players[1::2]) # pairs up players and stores pairs in a tuple
                 global nextround
                 nextround = [None] * (len(players)//2) # how many matches there will be next round
+                global matches
+                matches = zip(players[::2], players[1::2]) # pairs up players and stores pairs in a tuple
                 await ctx.send("Tournament Started")
             else:
                 await ctx.send("Usage Format: (Discord Name)#(numbers)|+")
 
     @commands.command(name='win')
-    async def win(self, ctx, name=None):
+    async def win(self, ctx, name=None, groupNumber=None):
+            groupnumber = int(groupNumber)
             if matches == ():
                 await ctx.send("There's no tournament")
             else:
                 if name in players:
-                    nextround.append(name) # this has issues when players complete their matches out of order of the bracket
-                    await ctx.send("Congrats {}, wait for your next match".format(name))
+                    nextround[groupnumber - 1] = name # this has issues when players complete their matches out of order of the bracket
+                    if len(nextround) == 1 and None not in nextround:
+                        await ctx.send("We have a winner")
+                    else:
+                        await ctx.send("Congrats {}, wait for your next match".format(name))
+                    if None not in nextround and len(nextround) != 1:
+                        global ready
+                        ready = True
+                        await ctx.send("Next round is ready")
+                    # await ctx.send("{}".format(nextround))
                 else:
                     await ctx.send("You're not in the tournament")
-            if None not in nextround:
-                global ready
-                ready = True
-                await ctx.send("Next round is ready")
-            elif len(nextround) == 1:
-                await ctx.send("We have a winner")
+
+
 
     @commands.command(name='nextround_start')
     async def nextround_start(self, ctx):
@@ -66,8 +71,6 @@ class Commands(commands.Cog):    # Class storing all the commands of the bot
                  = match[1]))
         else:
             await ctx.send("Next round is not ready yet")
-
-
 
 
 bot.add_cog(Commands())     # Adding the commands to the bot
