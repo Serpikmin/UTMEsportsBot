b9aec85e1c2b143c28300e2c1c3dd4b9ade2375b
NathanW001 <nathan.wootton1@gmail.com>
2022-05-01 00:35:07 -0400

    added start of users and json file

--- esports_bot.py
+++ esports_bot.py
@@ -1,9 +1,12 @@
 import os
 import string
 import discord
+import json
 from discord.ext import commands
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
+
+GAMES = ['Guilty Gear Strive', 'Street Fighter V', 'Tekken 7', 'Dragon Ball FighterZ', 'King of Fighers XV']
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,15 +16,23 @@ class Lobby:   # Class to store lobby information
         self.lobby_code = lobby_code
         self.lobby_time = datetime.now()  # Track when lobby was created
         self.creator = creator_name
-    
-    def get_code(self) -> string:
-        return self.lobby_code
 
+class User:
+    def __init__(self, user, ):
+        self.user = user
+        self.aliases = set()
+        self.games = set()
+
+users_have_changed = False # stores if users have been updated (to update json file)
 current_lobbies = set() # Store currently hosted lobbies, added by commands
+all_users = set() # Stores all users and also saves it to a file that we can load from
 
 bot = commands.Bot(command_prefix='!')  # Bot commands designated by !
 bot.remove_command("help")              # Removes the default help command.
 
+f=open('data.json')
+all_users = json.loads(f) # load values for json()
+f.close()
 
 class Commands(commands.Cog):    # Class storing all the commands of the bot
     @commands.command(name='hello')
@@ -39,10 +50,10 @@ class Commands(commands.Cog):    # Class storing all the commands of the bot
                 if ((current_time - lobby.lobby_time).total_seconds()/3600) > 24: # If lobby has been here for 24h, remove
                     current_lobbies.remove(lobby)
                 else:
-                    lobby_messages.append('**{}** created by {} at {}:{} on {}/{}'.format(lobby.lobby_code, lobby.creator, lobby.lobby_time.hour, lobby.lobby_time.minute, lobby.lobby_time.month, lobby.lobby_time.day))
+                    lobby_messages.append('**{}** created {}:{} {}/{} by {}'.format(lobby.lobby_code, lobby.lobby_time.hour, lobby.lobby_time.minute, lobby.lobby_time.month, lobby.lobby_time.day, lobby.creator))
             
             if current_lobbies == set():
-                lobby_messages.append('No lobbies right now :(')
+                lobby_messages.append('No lobbies right now D:')
 
             embed_val = '\n'.join(lobby_messages)
             embed.add_field(name='Guilty Gear Strive:', value=embed_val, inline=False)
@@ -52,6 +63,15 @@ class Commands(commands.Cog):    # Class storing all the commands of the bot
             new_lobby = Lobby(lobby_code, ctx.author.name)
             current_lobbies.add(new_lobby)
             await ctx.send('Lobby Added!')
+
+    async def setup_new_user(self, ctx):
+        print('wip lol')
+
+    @commands.command(name='tempsave')
+    async def save_users(self, ctx):
+        f=open('data.json', 'w')
+        f.write(json.dump(all_users))
+        f.close()
 
 bot.add_cog(Commands())     # Adding the commands to the bot
 bot.run(TOKEN)              # This runs the bot
