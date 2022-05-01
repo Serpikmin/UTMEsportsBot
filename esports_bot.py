2d694cd02afe4be8b7a6e2178edced2116087596
THLi3 <31786494+THLi3@users.noreply.github.com>
2022-04-30 21:51:33 -0400

    minor changes

--- esports_bot.py
+++ esports_bot.py
@@ -30,11 +30,11 @@ class Commands(commands.Cog):    # Class storing all the commands of the bot
         else:
             if re.match("^([a-zA-z]+#[0-9][0-9][0-9][0-9]/)+([a-zA-z]+#[0-9][0-9][0-9][0-9])$", brackets):
                 global players
-                players = brackets.split("/")
+                players = brackets.split("/") # stores players in a list
                 global matches
-                matches = zip(players[::2], players[1::2])
+                matches = zip(players[::2], players[1::2]) # pairs up players and stores pairs in a tuple
                 global nextround
-                nextround = [None] * (len(players)//2)
+                nextround = [None] * (len(players)//2) # how many matches there will be next round
                 await ctx.send("Tournament Started")
             else:
                 await ctx.send("Usage Format: (Discord Name)#(numbers)|+")
@@ -45,7 +45,7 @@ class Commands(commands.Cog):    # Class storing all the commands of the bot
                 await ctx.send("There's no tournament")
             else:
                 if name in players:
-                    nextround.append(name)
+                    nextround.append(name) # this has issues when players complete their matches out of order of the bracket
                     await ctx.send("Congrats {}, wait for your next match".format(name))
                 else:
                     await ctx.send("You're not in the tournament")
@@ -53,7 +53,7 @@ class Commands(commands.Cog):    # Class storing all the commands of the bot
                 global ready
                 ready = True
                 await ctx.send("Next round is ready")
-            if len(nextround) == 1:
+            elif len(nextround) == 1:
                 await ctx.send("We have a winner")
 
     @commands.command(name='nextround_start')
@@ -62,7 +62,8 @@ class Commands(commands.Cog):    # Class storing all the commands of the bot
             global matches
             matches = zip(nextround[::2], nextround[1::2])
             for match in matches:
-                await ctx.send("@{} and @{}".format(match[0], match[1]))
+                await ctx.send("@{player1} and @{player2}".format(player1 = match[0], player2
+                 = match[1]))
         else:
             await ctx.send("Next round is not ready yet")
 
